bb9dae048a5f005f16207bca8fc03fd15302a261
sword-ai <59247278+sword-ai@users.noreply.github.com>
2020-03-23 19:48:15 +0500

    Create username.py

---
+++ username.py
@@ -0,0 +1,7 @@
+username = input("welcome, please enter your username. ")
+password = input("please enter your password. ")
+
+if username != "saif" or password != "password123":
+    print("access denied")
+else:
+    print("access granted")
